cdf0f58fc616a0029a4c32a8e5a5d399983850b4
walter potma <145157901+waltindobne@users.noreply.github.com>
2025-03-20 17:47:21 -0300

    1

--- app/src/components/generator/page.tsx
+++ app/src/components/generator/page.tsx
@@ -19,6 +19,13 @@ interface DataQuest{
 function Page(){
     const router = useRouter();
     const { data, setData } = useData();
+    const [ numQuest, setNumQuest] = useState(1);
+    const [ vaga, setVaga] = useState('')
+    const [ anotacoes, setAnotacoes] = useState('')
+    const [ transcricao, setTranscricao] = useState('')
+    const [ curriculo, setCurriculo] = useState('')
+    const [ perguntas, setPerguntas] = useState([])
+
     const [formValues, setFormValues] = useState({
         vaga: "",
         curriculo: "",
@@ -55,18 +62,26 @@ function Page(){
     const returnHome = () => {
         router.push('/')
     }
-    const next = (e: React.FormEvent) => {
+    const handleChangeSubmit = (e: React.FormEvent) => {
         e.preventDefault(); // Impede o envio padrão do formulário
-    
+        useEffect(() => {
+            InovaService.gerarQuest(numQuest, vaga, perguntas)
+                .then((response) => {
+                    console.log(response.data)
+                })
+                .catch((error) => {
+                    console.error('Alguma coisa nessa budega:', error);
+                });
+        });
         // Atualiza os dados no contexto antes de redirecionar
         setData({
             ...data,
-            vaga: formValues.vaga,
-            curriculo: formValues.curriculo,
-            anotacoes: formValues.anotacoes,
-            transcricao: formValues.transcricao,
+            vaga: vaga,
+            curriculo: curriculo,
+            anotacoes: anotacoes,
+            transcricao: transcricao,
             relatorios: formValues.relatorios,
-            perguntas: formValues.perguntas ? [formValues.perguntas] : [],
+            perguntas: perguntas,
             respostas: formValues.respostas
         });
     
@@ -75,7 +90,7 @@ function Page(){
         // Redireciona para a página de perguntas
         router.push('/questions');
     };
-    
+
 
     return (
         <div className={styles.main}>
@@ -83,17 +98,29 @@ function Page(){
             <button className={styles.returnHome} onClick={returnHome}><ArrowBigLeft/>Home</button>
             <div>
                 <div className={styles.bodyInsert}>
-                    <form method="post" onSubmit={next} className={styles.formGenerator}>
+                    <form method="post" onSubmit={handleChangeSubmit} className={styles.formGenerator}>
                         <textarea 
                             name="vaga" 
                             id="" 
                             placeholder="Descreva sua vaga aqui" 
-                            value={formValues.vaga} 
-                            onChange={(e) => setFormValues({ ...formValues, vaga: e.target.value })}
+                            value={vaga} 
+                            onChange={(e) => setVaga(e.target.value)}
                         />
                         <div className={styles.descricao}>
-                            <input type="text" placeholder="Anotações"  className={styles.input}/>
-                            <input type="text" placeholder="Trasncrição" className={styles.input} />
+                            <input 
+                                type="text"
+                                placeholder="Anotações"
+                                className={styles.input}
+                                value={anotacoes} 
+                                onChange={(e) => setAnotacoes(e.target.value)}
+                            />
+                            <input
+                                type="text"
+                                placeholder="Transcrição"
+                                className={styles.input}
+                                value={transcricao} 
+                                onChange={(e) => setTranscricao(e.target.value)}
+                            />
                         </div>
                         <div className={styles.descricao}>
                             {/*
@@ -102,7 +129,13 @@ function Page(){
                                 Anexar Curriculo Base
                                 <input type="file" className={styles.fileCV} hidden/>
                             </label>*/}
-                            <input type="text" className={styles.fileCV} placeholder="Curriculo" />
+                            <input
+                                type="text"
+                                className={styles.fileCV}
+                                placeholder="Curriculo" 
+                                value={curriculo} 
+                                onChange={(e) => setCurriculo(e.target.value)}
+                            />
                             {/*<input type="number" placeholder="Descreva a vaga" required className={styles.quantCandidatos}/>*/}
                             <button type="submit"><SendHorizonal/></button>
                         </div>
